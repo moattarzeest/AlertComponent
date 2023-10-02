4b7c41b95658ec2481fcdb5273545df1b770473a
Moattar Zeest Rana <90420556+moattarzeest@users.noreply.github.com>
2023-10-02 15:40:40 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Alerts from './components/Alerts'
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleSendClick = () => {
+    if (inputValue.trim() !== '') {
+      setShowAlert(true);
+    }
+  };
   return (
     <div className="App">
-    <Alerts/>
+    <input
+        type="text"
+        placeholder="Enter text..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleSendClick}>Send</button>
+      {showAlert && <Alerts />}
     </div>
   );
 }
